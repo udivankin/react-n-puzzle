cdd754f19d09c7fd4321c1f3bc9e9dbcc3ca0e8d
allx <allx@mail.ru>
2020-05-20 21:19:06 +0300

    Updates

---
+++ precache-manifest.54eb4bf2a13f6173b9486cdf04536b11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc0f81785778b5b977789ad22a70d6a",
+    "url": "/react-n-puzzle/index.html"
+  },
+  {
+    "revision": "de71e965a83e2dd3e4d8",
+    "url": "/react-n-puzzle/static/css/main.07326fe4.chunk.css"
+  },
+  {
+    "revision": "2096a3a8576258570ffa",
+    "url": "/react-n-puzzle/static/js/2.99ffb063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-n-puzzle/static/js/2.99ffb063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de71e965a83e2dd3e4d8",
+    "url": "/react-n-puzzle/static/js/main.6ec4f5ac.chunk.js"
+  },
+  {
+    "revision": "93273f15d2b95b41d5d5",
+    "url": "/react-n-puzzle/static/js/runtime-main.cdb65590.js"
+  },
+  {
+    "revision": "cc945de37f5cb2ecf92e01bd12525bd5",
+    "url": "/react-n-puzzle/static/media/shuffle.cc945de3.svg"
+  },
+  {
+    "revision": "86b945566377ce3554f4b835f9b6e5c5",
+    "url": "/react-n-puzzle/static/media/star.86b94556.svg"
+  },
+  {
+    "revision": "71069831c4a8cabc2852c6f3367f01bf",
+    "url": "/react-n-puzzle/static/media/win.71069831.ogg"
+  }
+]);
